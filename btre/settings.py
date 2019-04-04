2e856e892ee1e1bb9527e4c8d8067427fe3749d6
Nicolas El-Hage <nicolas@delegateit.co.za>
2019-04-04 18:30:54 +0200

    added local settings

--- btre/settings.py
+++ btre/settings.py
@@ -146,3 +146,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nicelhage@gmail.com'
 EMAIL_HOST_PASSWORD = 'Nic12345'
 EMAIL_USE_TLS = 'TRUE'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
